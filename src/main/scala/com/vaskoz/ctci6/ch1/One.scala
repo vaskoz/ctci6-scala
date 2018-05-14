03c9f0f148c12b18487a06055a3d92779eb38b17
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-14 14:59:25 -0600

    don't need to make these executable applications

--- src/main/scala/com/vaskoz/ctci6/ch1/One.scala
+++ src/main/scala/com/vaskoz/ctci6/ch1/One.scala
@@ -1,5 +1,5 @@
 package com.vaskoz.ctci6.ch1
 
-object One extends App {
+object One {
   def isUnique(str: String) : Boolean = str == str.distinct
 }
--- src/main/scala/com/vaskoz/ctci6/ch1/Two.scala
+++ src/main/scala/com/vaskoz/ctci6/ch1/Two.scala
@@ -1,5 +1,5 @@
 package com.vaskoz.ctci6.ch1
 
-object Two extends App {
+object Two {
   def isPermutation(s1: String, s2: String) : Boolean = s1.sorted == s2.sorted
 }
