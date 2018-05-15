d55909587d0462266e8c60ed36811651a838b254
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-14 22:40:32 -0600

    yet another codacy style issue

--- src/main/scala/com/vaskoz/ctci6/ch1/Four.scala
+++ src/main/scala/com/vaskoz/ctci6/ch1/Four.scala
@@ -4,11 +4,11 @@ object Four {
   def couldBePalindrome(str: String): Boolean = oddOccurences(str).size <= 1
 
   def oddOccurences(str: String): List[(Char, Int)] = {
-    val groupByLower = noSpaces(str).groupBy(x => x.toLower)
-    val countOccurencesMap = groupByLower.map(x => (x._1, x._2.length))
+    val groupByLower = noSpaces(str).groupBy(char => char.toLower)
+    val countOccurencesMap = groupByLower.map(countTuple => (countTuple._1, countTuple._2.length))
     val countOccurences = countOccurencesMap.toList
-    val oddCountsOnly = countOccurences.filter(x => x._2 % 2 != 0)
-    return oddCountsOnly
+    val oddCountsOnly = countOccurences.filter(countTuple => countTuple._2 % 2 != 0)
+    oddCountsOnly
   }
 
   def noSpaces(str: String): String = str.filterNot(x => x.isSpaceChar)
