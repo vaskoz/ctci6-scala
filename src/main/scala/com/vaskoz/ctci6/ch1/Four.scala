29d0921e687b61a916b7139195e07ac98a7b8483
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-05-14 22:31:16 -0600

    refactor ch1q4 for codacy style

--- src/main/scala/com/vaskoz/ctci6/ch1/Four.scala
+++ src/main/scala/com/vaskoz/ctci6/ch1/Four.scala
@@ -3,9 +3,11 @@ package com.vaskoz.ctci6.ch1
 object Four {
   def couldBePalindrome(str: String): Boolean = oddOccurences(str).size <= 1
 
-  def oddOccurences(str: String): List[(Char, Int)] = countOccurrences(str).filter(x => x._2 % 2 != 0)
-
-  def countOccurrences(str: String): List[(Char, Int)] = noSpaces(str).groupBy(x => x.toLower).map(x => (x._1, x._2.length)).toList
-
+  def oddOccurences(str: String): List[(Char, Int)] = {
+    val groupByLower = noSpaces(str).groupBy(x => x.toLower)
+    val countOccurences = groupByLower.map(x => (x._1, x._2.length)).toList
+    countOccurences.filter(x => x._2 % 2 != 0)
+  }
+  
   def noSpaces(str: String): String = str.filterNot(x => x.isSpaceChar)
 }
